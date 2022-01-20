614e1e8f9eba4bcb755314a87751dae7053a8c39
Alice Lui <alice.lui@digital.homeoffice.gov.uk>
2022-01-20 09:07:20 +0000

    Set up knex schema

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'hof_example_app_form',
+      user:     'knex',
+      password: 'knex'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ migrations/20220119170635_forms.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('forms', table => {
+    table.increments();
+    table.string('email').notNullable();
+    table.json('session').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('forms');
+};
